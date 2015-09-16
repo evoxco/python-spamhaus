763f0f749193492cbedc00cf40de8ec8fd6e992e
etz69 <dev@evoxco.com>
2015-09-16 14:16:06 +0400

    Minor update

--- spamhaus.py
+++ spamhaus.py
@@ -12,36 +12,56 @@ This program is free software: you can redistribute it and/or modify
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Copyright 2014 Evox Computing Ltd
+    2014 Evox Computing Ltd
 """
 
 import dns.resolver
-import dns.reversename
 
 zen_return_codes = {'127.0.1.2': 'Spam domain',
-                     '127.0.1.4': 'Phishing domain',
-                     '127.0.1.5': 'Malware domain',
-                     '127.0.1.6': 'Botnet C&C domain',
-                     '127.0.1.102': 'Abused legit spam',
-                     '127.0.1.103': 'Abused spammed redirector domain',
-                     '127.0.1.104': 'Abused legit phish',
-                     '127.0.1.105': 'Abused legit malware',
-                     '127.0.1.106': 'Abused legit botnet C&C',
-                     '127.0.1.255': 'IP queries prohibited!',
-                     '127.0.0.2': 'Static UBE sources, verified spam services',
-                     '127.0.0.3': 'Static UBE sources, verified spam services',
-                     '127.0.0.4': 'Illegal 3rd party exploits, including'
+                    '127.0.1.4': 'Phishing domain',
+                    '127.0.1.5': 'Malware domain',
+                    '127.0.1.6': 'Botnet C&C domain',
+                    '127.0.1.102': 'Abused legit spam',
+                    '127.0.1.103': 'Abused spammed redirector domain',
+                    '127.0.1.104': 'Abused legit phish',
+                    '127.0.1.105': 'Abused legit malware',
+                    '127.0.1.106': 'Abused legit botnet C&C',
+                    '127.0.1.255': 'IP queries prohibited!',
+                    '127.0.0.2': 'Static UBE sources, verified spam services',
+                    '127.0.0.3': 'Static UBE sources, verified spam services',
+                    '127.0.0.4': 'Illegal 3rd party exploits, including'
                                   ' proxies, worms and trojan exploits',
-                     '127.0.0.5': 'Illegal 3rd party exploits, including'
+                    '127.0.0.5': 'Illegal 3rd party exploits, including'
                                   ' proxies, worms and trojan exploits',
-                     '127.0.0.6': 'Illegal 3rd party exploits, including'
+                    '127.0.0.6': 'Illegal 3rd party exploits, including'
                                   ' proxies, worms and trojan exploits',
-                     '127.0.0.7': 'Illegal 3rd party exploits, including'
+                    '127.0.0.7': 'Illegal 3rd party exploits, including'
                                   ' proxies, worms and trojan exploits',
-                     '127.0.0.10': 'IP ranges which should not be delivering'
+                    '127.0.0.10': 'IP ranges which should not be delivering'
                                    ' unauthenticated SMTP email',
-                     '127.0.0.11': 'IP ranges which should not be delivering'
+                    '127.0.0.11': 'IP ranges which should not be delivering'
                                    ' unauthenticated SMTP email'
+
+                    }
+
+zen_return_codes_cif = {'127.0.1.2': 'Spam',
+                    '127.0.1.4': 'Phishing',
+                    '127.0.1.5': 'Malware',
+                    '127.0.1.6': 'Botnet',
+                    '127.0.1.102': 'Spam',
+                    '127.0.1.103': 'Spam',
+                    '127.0.1.104': 'Phishing',
+                    '127.0.1.105': 'Malware',
+                    '127.0.1.106': 'Botnet',
+                    '127.0.1.255': 'Unknown!',
+                    '127.0.0.2': 'Spam',
+                    '127.0.0.3': 'Spam',
+                    '127.0.0.4': 'Exploit',
+                    '127.0.0.5': 'Exploit',
+                    '127.0.0.6': 'Exploit',
+                    '127.0.0.7': 'Exploit',
+                    '127.0.0.10': 'Dynamic IP',
+                    '127.0.0.11': 'Dynamic IP'
 
                     }
 
@@ -65,7 +85,9 @@ class SpamhausChecker:
 
         try:
 
+
             _r_name = dns.reversename.from_address(ip_address)
+
             _r_name = str(_r_name).replace("in-addr.arpa.", "zen.spamhaus.org.")
             answers = sp_resolver.query(_r_name)
 
@@ -74,6 +96,8 @@ class SpamhausChecker:
                 self.sp_response = {'status': '1',
                                     'response_code':
                                     zen_return_codes[rdata.address],
+                                    'assessment':
+                                    zen_return_codes_cif[rdata.address],
                                     'url': _url}
         except Exception, e:
             pass
@@ -83,4 +107,7 @@ class SpamhausChecker:
 if __name__ == "__main__":
 
     sh = SpamhausChecker()
-    response = sh.check_status("58.119.19.2")
+    response = sh.check_status("125.77.17.5")
+
+
+    print response
